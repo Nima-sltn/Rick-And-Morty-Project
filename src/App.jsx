4be3939e34c3cb34e78af81f2fe13c760a1ec410
Nima-sltn <nimaso8228@gmail.com>
2024-02-19 21:41:24 +0330

    feat: characterList component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import CharacterList from "./components/characterList";
+import { allCharacters } from "../data/data";
 
 function App() {
   return (
     <>
       <div className="app">
         <Navbar />
+        <div className="main">
+          <CharacterList characters={allCharacters} />
+        </div>
       </div>
     </>
   );
