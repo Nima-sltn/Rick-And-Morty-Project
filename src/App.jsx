b2fe17ea1fa55d70c5f261248f583bf0f25a3dae
Nima-sltn <nimaso8228@gmail.com>
2024-02-29 18:31:45 +0330

    refactor: refactored API requests to use axios for improved request handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import { allCharacters } from "../data/data";
 import Navbar from "./components/Navbar";
 import CharacterList from "./components/characterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -11,18 +12,44 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      setIsLoading(true);
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await res.json();
-      setCharacters(data.results.slice(0, 5));
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          "https://rickandmortyapi.com/api/characterss"
+        );
+        setCharacters(data.results.slice(0, 5));
+      } catch (err) {
+        toast.error(err.response.data.error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
   }, []);
 
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true);
+  //       const res = await fetch("https://rickandmortyapi.com/api/characterss");
+
+  //       if (!res.ok) throw new Error("something went wrong");
+
+  //       const data = await res.json();
+  //       setCharacters(data.results.slice(0, 5));
+  //     } catch (err) {
+  //       toast.error(err.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <div className="app">
+        <Toaster />
         <Navbar numOfResult={characters.length} />
         <div className="main">
           <CharacterList characters={characters} isLoading={isLoading} />
